201a99b0b6c43b8ddd27c29beb49ea0a6bb926b8
JoanChege <jncheg@gmail.com>
2024-04-29 18:07:24 +0300

    add db connection

--- backend/server.js
+++ backend/server.js
@@ -3,46 +3,42 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const multer = require('multer');
 const upload = multer();
-
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
-let dbName = 'yolomy';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://jncheg:joannalee@cluster0.te6tjxb.mongodb.net/yolomy';
 
-// define a url to connect to the database
-const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
-mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
-let db = mongoose.connection;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
 
 // Check Connection
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
+db.once('open', () => {
+    console.log('Database connected successfully');
+});
 
 // Check for DB Errors
-db.on('error', (error)=>{
-    console.log(error);
-})
+db.on('error', (error) => {
+    console.error('Database connection error:', error);
+});
 
 // Initializing express
-const app = express()
+const app = express();
 
 // Body parser middleware
-app.use(express.json())
+app.use(express.json());
 
-// 
-app.use(upload.array()); 
+// Multer middleware
+app.use(upload.array());
 
 // Cors 
 app.use(cors());
 
 // Use Route
-app.use('/api/products', productRoute)
+app.use('/api/products', productRoute);
 
 // Define the PORT
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
-    console.log(`Server listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
